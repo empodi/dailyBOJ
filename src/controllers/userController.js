97cba3a02c2584458afab1b09bce8be3c5587962
empodi <kglein9513@gmail.com>
2022-08-17 22:26:08 +0900

    회원가입 비밀번호 crypto

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/User";
 import axios from "axios";
+import crypto from "crypto";
 
 const baseOption = {
   method: "GET",
@@ -8,6 +9,24 @@ const baseOption = {
   headers: { "Content-Type": "application/json" },
 };
 
+const createSalt = () =>
+  new Promise((resolve, reject) => {
+    crypto.randomBytes(64, (err, buf) => {
+      if (err) reject(err);
+      resolve(buf.toString("base64"));
+    });
+  });
+
+const createHashedPassword = (plainPassword) =>
+  // eslint-disable-next-line no-async-promise-executor
+  new Promise(async (resolve, reject) => {
+    const salt = await createSalt();
+    crypto.pbkdf2(plainPassword, salt, 9999, 64, "sha512", (err, key) => {
+      if (err) reject(err);
+      resolve({ hashedPassword: key.toString("base64"), salt });
+    });
+  });
+
 export const getJoin = (req, res) => {
   return res.render("join");
 };
@@ -15,6 +34,16 @@ export const getJoin = (req, res) => {
 export const postJoin = async (req, res) => {
   const { userId, password, confirmPassword, email } = req.body;
   console.log(userId, password, confirmPassword, email);
+
+  console.log("userId", userId);
+  if (userId.length < 3) {
+    return res.status(400).render("join");
+  }
+  if (password !== confirmPassword) {
+    return res.status(400).render("join");
+  }
+
+  const { hashedPassword, salt } = await createHashedPassword(password);
 
   baseOption.params.query = `solved_by:${userId}`;
 
@@ -31,19 +60,18 @@ export const postJoin = async (req, res) => {
       totalSolved.push(Number(item.problemId));
     }
   }
-  console.log(i);
 
   try {
     await User.create({
       userId,
-      password,
+      password: hashedPassword,
+      salt,
       email,
       totalSolved,
     });
+    return res.redirect("/login");
   } catch (err) {
     console.log(err);
     return res.status(400).render("join");
   }
-
-  return res.send("LOGIN POST");
 };
